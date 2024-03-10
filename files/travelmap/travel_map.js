21c049d84fb3afd75896ac6022d09fa077559a7e
Andrew-Jones657 <148402509+Andrew-Jones657@users.noreply.github.com>
2024-03-10 03:20:34 -0500

    Update travel_map.js

--- files/travelmap/travel_map.js
+++ files/travelmap/travel_map.js
@@ -2,10 +2,11 @@
 	const map = L.map("map").setView([36.5980, -38.4648], 4);
 
 	// Load in the basemap -- OpenStreetMaps
-	const tiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-		maxZoom: 17,
-		attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
-		opacity: 0.90
+	const tiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
 	}).addTo(map);
 
 	// Create a fullscreen button
