ede8a7fe1dd359e00bcf57edaffe6ded4047f83a
Andrew-Jones657 <148402509+Andrew-Jones657@users.noreply.github.com>
2024-03-11 02:30:23 -0500

    Update travel_map.js

--- files/travelmap/travel_map.js
+++ files/travelmap/travel_map.js
@@ -1,3 +1,22 @@
+/* 
+ * Workaround for 1px lines appearing in some browsers due to fractional transforms
+ * and resulting anti-aliasing.
+ * https://github.com/Leaflet/Leaflet/issues/3575
+ */
+	(function(){
+    	   var originalInitTile = L.GridLayer.prototype._initTile
+   	   L.GridLayer.include({
+           _initTile: function (tile) {
+               originalInitTile.call(this, tile);
+
+            var tileSize = this.getTileSize();
+
+            tile.style.width = tileSize.x + 1 + 'px';
+            tile.style.height = tileSize.y + 1 + 'px';
+        	}
+    	});
+	})()
+
 	// Disable translate3d
 	L_DISABLE_3D = true;
 
