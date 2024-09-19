227a28693eb455f8f7199d2b3c338c018aaa3823
Andrew-Jones657 <148402509+Andrew-Jones657@users.noreply.github.com>
2024-09-19 00:35:04 -0500

    Create KrigingJUN99Histogram.R

---
+++ files/R_Examples/KrigingJUN99Histogram.R
@@ -0,0 +1,45 @@
+library(ggplot2)
+library(tidyverse)
+library(hrbrthemes)
+library(magrittr)
+library(extrafont)
+install.packages(ggpubr)
+
+
+
+drought_data
+
+fonts()
+
+loadfonts()
+font_import()
+windowsFonts()
+
+
+
+
+library(tidyverse)
+library(hrbrthemes)
+library(extrafont)
+library(magrittr)
+
+h <- drought_data %>%
+  filter( JUN99 < 10) %>%
+  ggplot( aes(x=JUN99)) +
+    geom_histogram( bins=8, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+    labs(
+      title = "Histogram of Kentucky Cumulative Drought Anomalies",
+      subtitle = "July 1998 - June 1999",
+      caption = "Histogram prepared by Andrew Jones",
+      x = "Cumulative Drought Values (in.)",
+      y = "Frequency") +
+	theme(text=element_text(
+	family="Arial", 
+	face="bold", 
+	size=12)) + 
+	theme(panel.grid.major = element_blank(), 
+	panel.grid.minor = element_blank(),
+	panel.background = element_blank(), 
+	axis.line = element_line(colour = "black"))
+
+h
