6ad66617fc0fa0d16a14b5dd5470ec68fa2dde64
Andrew-Jones657 <148402509+Andrew-Jones657@users.noreply.github.com>
2024-11-01 15:31:21 -0500

    Create MultivariateSilhouetteWidthPlot.R

---
+++ files/R_Examples/MultivariateSilhouetteWidthPlot.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+library(factoextra)
+library(tidyverse)
+library(parameters)
+library(cluster)
+
+# Read in the drought dataset. It contains unique identifiers, names, lat-lon values, and the drought anomalies for each month (Jul 98 - Jun 01) 
+drought.data.raw <- read.csv("path_to_transposed_dataset")
+
+
+# Remove unncessary information so the data can be scaled 
+trim_matrix <- drought.data.raw[, !colnames(drought.data.matrix) %in% c("FID_", "CDN", "LON", "LAT" )]
+
+# Transform the location name into the row name labels
+m <- trim_matrix %>% remove_rownames %>% column_to_rownames(var="NAME")
+
+# Scale the data 
+m_scale <- scale(m)
+
+# Use silhouette plot to confirm choice: 2 = 0.41, 3 = 0.36, 4 = 0.28, 5 = 0.24, 6 = 0.26, 7 = 0.25, 8 = 0.27
+km_res <- kmeans(m_scale, centers = 2, nstart = 20)
+
+sil <- silhouette(km_res$cluster, dist(m))
+fviz_silhouette(sil)
+
+# Observe which observations have negative silhouette widths
+
+neg_sil_index <- which(sil[, "sil_width"] < 0)
+sil[neg_sil_index, , drop = FALSE]
