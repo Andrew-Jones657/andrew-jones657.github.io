bc8ec21c432518e7ea5b741ac729e4bae3fe7948
Andrew-Jones657 <148402509+Andrew-Jones657@users.noreply.github.com>
2024-12-05 12:38:23 -0600

    Update KrigingJUN99QQPlot.R

--- files/R_Examples/KrigingJUN99QQPlot.R
+++ files/R_Examples/KrigingJUN99QQPlot.R
@@ -1,27 +1,11 @@
 library(ggplot2)
 
 
-drought_data <- read.csv("my system path to csv file")
+drought_data <- read.csv("path_to_dataset")
 
+shapiro.test(drought_data$JUN99)
 
-
-q <- drought_data %>%
-  filter( JUN99 < 10 %>%
-  ggplot(aes(x=JUN99)) +
-    qqplot(fill="#69b3a2", color="#e9ecef", alpha=0.9) + stat_qq() +
-    labs(
-      title = "Kentucky Cumulative Drought Anomalies QQPlot",
-      subtitle = "July 1998 - June 1999",
-      caption = "QQPlot prepared by Andrew Jones",
-      x = "Theoretical",
-      y = "") +
-	theme(panel.grid.major = element_blank(), 
-	panel.grid.minor = element_blank(),
-	panel.background = element_blank(), 
-	axis.line = element_line(colour = "black")) +
-	geom_abline(aes(slope = 5, intercept = 0), linetype = 2)
-
-ggplot(drought_data, aes(sample=JUN99)) + 
+ggplot(drought_data, aes(sample=JUN99)) + # Create a QQ plot with ggplot
 	stat_qq(fill="#87AC99", color="#87AC99", alpha=0.9) +
 	stat_qq_line() +
 	theme(text=element_text(
@@ -46,6 +30,5 @@ ggplot(drought_data, aes(sample=JUN99)) +
 	panel.background = element_blank(), 
 	axis.line = element_line(colour = "black"))
 
-q
 
-shapiro.test(drought_data$JUN99)
+
