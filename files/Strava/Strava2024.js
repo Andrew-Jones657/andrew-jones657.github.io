e260c07804bab78b7421e000fcb3fd08df87a47b
Andrew-Jones657 <148402509+Andrew-Jones657@users.noreply.github.com>
2024-12-10 19:04:35 -0600

    Update Strava2024.js

--- files/Strava/Strava2024.js
+++ files/Strava/Strava2024.js
@@ -146,7 +146,7 @@
         		});
 
         // add the UI for a title
-        view.ui.add("titleDiv", "bottom-left");
+        view.ui.add("titleDiv", "top-left");
 	
 	// add a fullscreen button
        const fullscreen = new Fullscreen({
