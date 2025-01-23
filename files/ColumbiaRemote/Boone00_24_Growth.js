d0636e0ed44fb89a1c41b40b286be2a4020a4864
Andrew-Jones657 <148402509+Andrew-Jones657@users.noreply.github.com>
2025-01-23 15:50:59 -0600

    Create Boone00_24_Growth.js

---
+++ files/ColumbiaRemote/Boone00_24_Growth.js
@@ -0,0 +1,15 @@
+    const map = L.map("map").setView([38.962576, -92.329513], 9);
+
+    const tiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	      minZoom: 5,
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+    const geojsonUrl = "https://raw.githubusercontent.com/Andrew-Jones657/andrew-jones657.github.io/refs/heads/main/files/ColumbiaRemote/BooneGrowth.geojson";
+
+    const booneUrl = "https://raw.githubusercontent.com/Andrew-Jones657/andrew-jones657.github.io/refs/heads/main/files/ColumbiaRemote/Boone.geojson";
+
+    const booneLayer = new L.GeoJSON.AJAX(booneUrl, {color: 'black'}).addTo(map);
+
+    const geojsonLayer = new L.GeoJSON.AJAX(geojsonUrl, {color: 'red'}).addTo(map);
