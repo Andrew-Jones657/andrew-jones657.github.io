6ceeeb60d63d3d9daa5e06b182e8c2466f8ed020
Andrew-Jones657 <148402509+Andrew-Jones657@users.noreply.github.com>
2025-02-07 08:15:25 -0600

    Update Boone00_24_Growth.js

--- files/ColumbiaRemote/Boone00_24_Growth.js
+++ files/ColumbiaRemote/Boone00_24_Growth.js
@@ -10,6 +10,14 @@
 
     const booneUrl = "https://raw.githubusercontent.com/Andrew-Jones657/andrew-jones657.github.io/refs/heads/main/files/ColumbiaRemote/Boone.geojson";
 
+    const boonePlaces00url = "https://raw.githubusercontent.com/Andrew-Jones657/andrew-jones657.github.io/refs/heads/main/files/ColumbiaRemote/BoonePlaces00.geojson";
+
+    const boonePlaces20url = "https://raw.githubusercontent.com/Andrew-Jones657/andrew-jones657.github.io/refs/heads/main/files/ColumbiaRemote/BoonePlaces20.geojson";
+
     const booneLayer = new L.GeoJSON.AJAX(booneUrl, {color: 'black'}).addTo(map);
+
+    const boonePlaces00 = new L.GeoJSON.AJAX(boonePlaces00url, {color: 'gold'}).addTo(map);
+
+    const boonePlaces20 = new L.GeoJSON.AJAX(boonePlaces20url, {color: 'green'}).addTo(map);
 
     const geojsonLayer = new L.GeoJSON.AJAX(geojsonUrl, {color: 'red'}).addTo(map);
