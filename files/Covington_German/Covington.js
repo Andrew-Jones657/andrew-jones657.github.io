daefa65feddcb243105a5306444fe8332d663b6a
Andrew-Jones657 <148402509+Andrew-Jones657@users.noreply.github.com>
2025-07-13 03:53:57 -0500

    Update Covington.js

--- files/Covington_German/Covington.js
+++ files/Covington_German/Covington.js
@@ -232,7 +232,7 @@
 
           const sliderLabel = document.createElement("calcite-label");
           sliderLabel.layout = "inline";
-          sliderLabel.innerHTML = "Opac.";
+          sliderLabel.innerHTML = " ";
           sliderLabel.appendChild(slider.container);
 
           const sliderDiv = document.createElement("div");
